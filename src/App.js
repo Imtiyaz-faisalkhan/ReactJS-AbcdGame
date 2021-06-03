1ad66bb988354833bbaef13dce20f3bcaaef2a48
Faisal Khan Imtiyaz <fimtiyaz@dotscreen.com>
2021-06-03 23:19:34 +0200

    HTML structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import EasyABC from './EasyABC'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>SetUp</h1>
+      <h1>ABCD Game</h1>
       </header>
+      <EasyABC/>
     </div>
   );
 }
